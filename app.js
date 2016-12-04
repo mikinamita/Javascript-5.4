6e905aa0f9dc909b5e6dae8b240fadc79bfdb156
mikinamita <mikinamita@gmail.com>
2016-12-04 09:32:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+var name="Namita";
+var age="30";
+var dob="28/06/1985";
+var pofb="Sundargarh";
+
+var person = {name:"Namita", age:"30", dob:"28/06/1985",pofb:"Sundargarh"}; 
+
+var employee = {name:"Namita", age:"30", salary:"5000",
+			address:{city:"Sundargarh",state:"Orissa",pin:"770072"}}; 
+			
+var employees=[]
+ employee[0]={name:"Namita", age:"30", salary:"5000",
+			address:{city:"Sundargarh",state:"Orissa",pin:"770072"}}; 
+ employee[1]={name:"Susmita", age:"31", salary:"6000",
+			address:{city:"South Delhi",state:"Delhi",pin:"110062"}}; 
+ employee[2]={name:"Mili", age:"32", salary:"5000",
+			address:{city:"Chandigarh",state:"Punjab",pin:"220022"}}; 
+ employee[3]={name:"Anita", age:"33", salary:"8000",
+			address:{city:"Gurgaon",state:"Haryana",pin:"770072"}}; 
+ employee[4]={name:"Banita", age:"34", salary:"7000",
+			address:{city:"Hyderabad",state:"AP",pin:"710072"}}; 
+
+
