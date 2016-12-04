61eb3a48c8a4e8871d31253f9ced5c95dc0b46ce
mikinamita <mikinamita@gmail.com>
2016-12-04 09:46:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var person = {name:"Namita", age:"30", dob:"28/06/1985",pofb:"Sundargarh"};
 var employee = {name:"Namita", age:"30", salary:"5000",
 			address:{city:"Sundargarh",state:"Orissa",pin:"770072"}}; 
 			
-var employees=[]
+var employee=[]
  employee[0]={name:"Namita", age:"30", salary:"5000",
 			address:{city:"Sundargarh",state:"Orissa",pin:"770072"}}; 
  employee[1]={name:"Susmita", age:"31", salary:"6000",
@@ -20,5 +20,22 @@ var employees=[]
 			address:{city:"Gurgaon",state:"Haryana",pin:"770072"}}; 
  employee[4]={name:"Banita", age:"34", salary:"7000",
 			address:{city:"Hyderabad",state:"AP",pin:"710072"}}; 
+			
+var i;
+
+for(i=1; i<=5;i++)
+{
+console.log("--------Detail of Employee No "+i+"---------" );
+console.log("Employee Name " +employee[i].name);
+console.log("Employee Age " +employee[i].age);
+console.log("Employee Salary " +employee[i].salary);
+console.log("Employee City " +employee[i].address.city);
+console.log("Employee State " +employee[i].address.state);
+console.log("Employee PinCode " +employee[i].address.pin);
+
+}
+			
+var radius=5;
+console.log("Radius of a Circle is "+3.14*radius*radius);
 
 
