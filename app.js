ce3d244259beab6dc538a0a5aaa89ad4abba5549
mikinamita <mikinamita@gmail.com>
2016-12-04 10:58:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-			
-//Assignment 5.4			
+//Assignment 6.4			
 var employee=[]
  employee[0]={name:"Namita", age:"30", salary:"5000",
 			address:{city:"Sundargarh",state:"Orissa",pin:"770072"}}; 
@@ -12,17 +11,44 @@ var employee=[]
  employee[4]={name:"Banita", age:"34", salary:"7000",
 			address:{city:"Hyderabad",state:"AP",pin:"710072"}}; 
 			
-var i;
 
-for(i=1; i<=5;i++)
-{
-console.log("--------Detail of Employee No "+i+"---------" );
-console.log("Employee Name " +employee[i].name);
-console.log("Employee Age " +employee[i].age);
-console.log("Employee Salary " +employee[i].salary);
-console.log("Employee City " +employee[i].address.city);
-console.log("Employee State " +employee[i].address.state);
-console.log("Employee PinCode " +employee[i].address.pin);
+console.log("--Employee 1-- ");
+console.log("Employee Name " +employee[0].name);
+console.log("Employee Age " +employee[0].age);
+console.log("Employee Salary " +employee[0].salary);
+console.log("Employee City " +employee[0].address.city);
+console.log("Employee State " +employee[0].address.state);
+console.log("Employee PinCode " +employee[0].address.pin);
 
-}
-			
+console.log("--Employee 2-- ");
+console.log("Employee Name " +employee[1].name);
+console.log("Employee Age " +employee[1].age);
+console.log("Employee Salary " +employee[1].salary);
+console.log("Employee City " +employee[1].address.city);
+console.log("Employee State " +employee[1].address.state);
+console.log("Employee PinCode " +employee[1].address.pin);
+
+console.log("--Employee 3-- ");
+console.log("Employee Name " +employee[2].name);
+console.log("Employee Age " +employee[2].age);
+console.log("Employee Salary " +employee[2].salary);
+console.log("Employee City " +employee[2].address.city);
+console.log("Employee State " +employee[2].address.state);
+console.log("Employee PinCode " +employee[2].address.pin);
+
+console.log("--Employee 4-- ");
+console.log("Employee Name " +employee[3].name);
+console.log("Employee Age " +employee[3].age);
+console.log("Employee Salary " +employee[3].salary);
+console.log("Employee City " +employee[3].address.city);
+console.log("Employee State " +employee[3].address.state);
+console.log("Employee PinCode " +employee[3].address.pin);
+
+console.log("--Employee 5-- ");
+console.log("Employee Name " +employee[4].name);
+console.log("Employee Age " +employee[4].age);
+console.log("Employee Salary " +employee[4].salary);
+console.log("Employee City " +employee[4].address.city);
+console.log("Employee State " +employee[4].address.state);
+console.log("Employee PinCode " +employee[4].address.pin);
+
